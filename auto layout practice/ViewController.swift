e0982b94fd5c89406e1ae85bda211ac2b8c6e905
Сергей Земсков <nailzed@yandex.ru>
2021-03-10 15:57:57 +0300

    Initial Commit

---
+++ auto layout practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto layout practice
+//
+//  Created by Сергей Земсков on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
